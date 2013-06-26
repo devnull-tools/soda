7fc81707ae93069e61d5b0f4a6efdba4e4f097ed
ataxexe <ataxexe@gmail.com>
2013-06-26 10:26:07 -0300

    changed require function to import

--- scripts/soda.sh
+++ scripts/soda.sh
@@ -7,15 +7,6 @@ function public {
   $(printf "%-30s" "${1//_/-}") $2"
 }
 
-function load_scripts {
-  if [[ -d "$1" ]]; then
-    for script in $(find $1 -type f -name "*.sh"); do
-      debug "Loading $script"
-      . $script
-    done
-  fi
-}
-
-function require {
+function import {
   load_scripts "$SODA_USER_DIR/scripts/$1"
 }
