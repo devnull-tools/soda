d3c7d7d333f5a683d2111a02909dc69cfc391535
ataxexe <ataxexe@gmail.com>
2013-07-03 17:31:17 -0300

    hide common namespace because it is imported by default

--- scripts/core.sh
+++ scripts/core.sh
@@ -67,9 +67,11 @@ SODA_IMPORTS=""
 #
 function import {
   if [[ ! $(echo "$SODA_IMPORTS" | grep -ie ":$1:") ]]; then
-    if [[ ! "$1" == "soda" ]]; then
-      TASK_NAMESPACE="$1$SODA_NAMESPACE_DELIMITER"
-      PARAMETER_NAMESPACE="[$1]"
+    if [[ "$1" != "soda" ]]; then
+      if [[ "$1" != "common" ]]; then
+        TASK_NAMESPACE="$1$SODA_NAMESPACE_DELIMITER"
+        PARAMETER_NAMESPACE="[$1]"
+      fi
     fi
     SODA_IMPORTS="$SODA_IMPORTS:$1:"
     NAMESPACES="$NAMESPACES $1"
